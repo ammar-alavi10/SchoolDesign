659d91b67632182a657a8d80936cc01ed132685d
ammar-alavi10 <superalavi20@gmail.com>
2020-07-21 18:28:32 +0530

    added balloon popper

---
+++ app/src/main/java/com/ammar/schooldesign/KidsCorner/balloonpoper/utils/PixelHelper.java
@@ -0,0 +1,12 @@
+package com.ammar.schooldesign.KidsCorner.balloonpoper.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+public class PixelHelper {
+    public static int pixelsToDp(int px, Context context) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, px,
+                context.getResources().getDisplayMetrics());
+    }
+}
